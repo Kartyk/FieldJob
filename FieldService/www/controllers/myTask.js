bd99bc1c1da7381bece43d20ea82f9ef6bb0d3ad
Kartyk <kartyksp@hmail.com>
2017-12-07 17:13:32 +0530

    Gayathri changes

--- FieldService/www/controllers/myTask.js
+++ FieldService/www/controllers/myTask.js
@@ -144,6 +144,10 @@ app.controller('myTaskController', function ($translate, $scope, $compile, $time
 
                     element.addClass("acceptedEvent");
 
+                } else if (event.Task_Status == 'Working') {
+
+                    element.addClass("workingEvent");
+
                 } else {
 
                     element.addClass("assignedEvent");
@@ -440,7 +444,7 @@ app.controller('myTaskController', function ($translate, $scope, $compile, $time
     $scope.onclickOfTask = function (task) {
 
         console.log("TASK " + JSON.stringify(task));
-
+      
         $scope.selectedTask = task;
 
         valueService.setTask(task, function (response) {
