ef573ee91ead4330e7ad04ba76ffcae5be0dc566
Kartyk <32518733+KotniAshutosh@users.noreply.github.com>
2017-12-26 18:21:32 +0530

    Fixes

--- FieldService/www/controllers/index.js
+++ FieldService/www/controllers/index.js
@@ -666,6 +666,12 @@ app.controller('indexController', function ($q, $scope, $state, $timeout, $mdSid
                 } else {
 
                     deferSubmit.resolve("Submit");
+
+                    if (isLogin == "0") {
+                        fetchData(isLogin);
+                    } else if (isLogin == "1") {
+                        syncData(isLogin);
+                    }
                 }
             });
 
